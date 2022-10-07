c74731d9802e2ae062752aa3a460e83b88aea082
BernardoHer <bernaher19@gmail.com>
2022-10-07 17:50:23 -0500

    fix bug

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 let btnContainer = document.querySelector('.btns-section');
 let rateNumber = document.querySelector('.thanks-section-user-select');
 let submitBtn = document.querySelector('.submit-button');
-let ratingSection =  document.querySelector('rating-section');
+let ratingSection =document.querySelector('.rating-section');
 let thanksSection = document.querySelector('.thanks-section');
 
-btnContainer.addEventListener('click', event =>{
-    let btnSelected = event.target.innerText;
-    rateNumber.innerText = btnSelected;
-    if(btnSelected >0 || btnSelected <=5){
-        submitBtn.addEventListener('click',()=>{
+
+
+
+btnContainer.addEventListener('click',event=>{
+    let numberSelected = event.target.innerText;
+    rateNumber.innerText = numberSelected;
+    if(numberSelected>0 || numberSelected <=5 ){
+        submitBtn.addEventListener('click', ()=>{
             ratingSection.style.display = 'none';
             thanksSection.style.display = 'flex';
-        })
+        
+        });
+
     }
 })
